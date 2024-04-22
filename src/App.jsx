2e42abbf106be3710fb18a7206ec15b8885d46d7
unknown <javierbiselli@gmail.com>
2024-04-22 13:00:16 -0300

    practica obligatoria 2 terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
-import Pelicula from "./components/Pelicula";
+import Table from "./components/Table";
 
 function App() {
-  return <Pelicula />;
+  const netIncomes = [
+    { brand: "McDonalds", income: 1291283 },
+    { brand: "Burger King", income: 1927361 },
+    { brand: "KFC", income: 1098463 },
+  ];
+
+  return <Table netIncomes={netIncomes} />;
 }
 
 export default App;
